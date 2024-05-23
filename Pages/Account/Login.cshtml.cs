4c3a99f0759e3f271f914abd9532c0f4bf97d135
Sebas y Juli <jgomez4967@campuscecep.edu.co>
2024-05-23 13:30:30 -0500

    autenticación en index

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SupermarketWEB.Models;
+using System.Security.Claims;
 
 namespace SupermarketWEB.Pages.Account
 {
@@ -18,6 +20,17 @@ namespace SupermarketWEB.Pages.Account
 
             if (User.Email == "correo@gmail.com" &&  User.Password == "12345")
             {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, User.Email),
+                };
+
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
                 return RedirectToPage("/index");
             }
             return Page();
