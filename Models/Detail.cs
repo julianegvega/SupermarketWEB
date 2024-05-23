86b482a5a2d80b343ec15d67e4e8f9b54db70fd8
Sebas y Juli <jgomez4967@campuscecep.edu.co>
2024-05-23 00:34:03 -0500

    Iindex detail

--- Models/Detail.cs
+++ Models/Detail.cs
@@ -7,9 +7,9 @@
         public int Price { get; set; }
 
         public int InvoiceId { get; set; }
-        public Invoice Invoice { get; set; }
+        public Invoice? Invoice { get; set; } = default!;
 
         public int ProductId { get; set; }
-        public Product Product { get; set; }
+        public Product? Product { get; set; } = default!;
     }
 }
