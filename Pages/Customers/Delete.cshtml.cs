b385807afa9184f51a57c7a7280f5bf8bca5d2fe
Sebas y Juli <jgomez4967@campuscecep.edu.co>
2024-05-23 00:31:31 -0500

    eliminar cliente

---
+++ Pages/Customers/Delete.cshtml.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Customers
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public DeleteModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public Customer Customer { get; set; } = default!;
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Customer = customer;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+            var customer = await _context.Customers.FindAsync(id);
+
+            if (customer != null)
+            {
+                Customer = customer;
+                _context.Customers.Remove(Customer);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
